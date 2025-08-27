0fc696725db9fb75500281cc51f2e53c608382ea
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2025-08-27 11:02:43 +0300

    sync new changes with

--- lazyvim/lua/plugins/avante.lua
+++ lazyvim/lua/plugins/avante.lua
@@ -1,17 +1,17 @@
 return {
-  "yetone/avante.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = { file_types = { "markdown", "Avante" } },
-      ft = { "markdown", "Avante" },
-    },
-  },
-  build = "make",
-  -- auth setup up using copilot
-  opts = { provider = "copilot" },
+  -- "yetone/avante.nvim",
+  -- dependencies = {
+  --   "nvim-tree/nvim-web-devicons",
+  --   "stevearc/dressing.nvim",
+  --   "nvim-lua/plenary.nvim",
+  --   "MunifTanjim/nui.nvim",
+  --   {
+  --     "MeanderingProgrammer/render-markdown.nvim",
+  --     opts = { file_types = { "markdown", "Avante" } },
+  --     ft = { "markdown", "Avante" },
+  --   },
+  -- },
+  -- build = "make",
+  -- -- auth setup up using copilot
+  -- opts = { provider = "copilot" },
 }
--- readme.md
+++ readme.md
@@ -56,3 +56,15 @@ Note, .stow-local-ignore ignore some non dotfiles
 # nvim
 with stow `~/.config/nvim` link to here `.config/nvim`
 which links to one of multiple setups ./current-nvim/ or ./vanilla-nvim/
+
+---
+Be Portable as Possible
+- [ ] move all neovim config in one file
+- [ ] list all the services/dependancies you want in a new setup
+- [ ] a way to sync services settings like gnome/browser/vscode
+- [ ] init.sh script automates all installation and configurations
+
+
+Try new
+- [ ] stow, chezmoi, dotbot, nixHomeManager
+- [ ] usable VIM config
